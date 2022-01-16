bf841859c618eed44d01d664b41d7a571fcac3c5
Gabriel Marinho <gdcmarinho@gmail.com>
2022-01-16 02:58:27 -0300

    feat(#7): add prettier configs

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import { TimePreferences } from "./src/model/TimePreferences";
+import { TimePreferences } from './src/model/TimePreferences';
 
 const DEFAULT_TIME_PREFERENCES: TimePreferences = {
     timeout: 3000,
     timeRetry: 30000,
-    progressiveRetry: false
-}
+    progressiveRetry: false,
+};
 
 const CLOSED = Symbol('closed');
 const OPEN = Symbol('open');
@@ -16,10 +16,14 @@ export class CircuitBreaker {
 
     constructor(act: Function, timePreferences?: TimePreferences) {
         this.currentState = CLOSED;
-        !timePreferences ? this.timePreferences = DEFAULT_TIME_PREFERENCES : this.timePreferences = timePreferences;
+        !timePreferences
+            ? (this.timePreferences = DEFAULT_TIME_PREFERENCES)
+            : (this.timePreferences = timePreferences);
 
         if (!act)
-            throw new Error('No act was informed. Please enter a method to be executed.');
+            throw new Error(
+                'No act was informed. Please enter a method to be executed.'
+            );
 
         this.executeAct(act, this.timePreferences, false);
     }
@@ -32,35 +36,41 @@ export class CircuitBreaker {
         this.currentState = newState;
     }
 
-    executeAct(act: Function, timePreferences: TimePreferences, isHalfOpen: boolean): void {
+    executeAct(
+        act: Function,
+        timePreferences: TimePreferences,
+        isHalfOpen: boolean
+    ): void {
         const res = act();
 
         if (res >= 500) {
             this.updateState(OPEN);
-            console.log("Service Unavailable", res);
+            console.log('Service Unavailable', res);
 
             setTimeout(() => {
-                if (isHalfOpen)
-                    this.updateState(HALF_OPEN);
+                if (isHalfOpen) this.updateState(HALF_OPEN);
 
-                this.executeAct(act, timePreferences, true)
+                this.executeAct(act, timePreferences, true);
             }, timePreferences.timeRetry);
         } else {
             this.updateState(CLOSED);
 
             return res;
         }
-    };
+    }
 }
 
 const EXAMPLE_ACT = function mockExternalServiceReturn(): number {
     return 200;
-}
+};
 
 const EXAMPLE_TIME_PREFERENCES: TimePreferences = {
     timeout: 3000,
     timeRetry: 10000,
-    progressiveRetry: false
-}
+    progressiveRetry: false,
+};
 
-const circuitBreaker = new CircuitBreaker(EXAMPLE_ACT, EXAMPLE_TIME_PREFERENCES);
+const circuitBreaker = new CircuitBreaker(
+    EXAMPLE_ACT,
+    EXAMPLE_TIME_PREFERENCES
+);
