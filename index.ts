f1c2c7725ab70f4de7bae0d01fce37ea9f22d316
Gabriel Marinho <gdcmarinho@gmail.com>
2022-01-16 03:54:39 -0300

    fix: change mock return again

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { CircuitBreaker } from './src/CircuitBreaker';
 import { TimePreferences } from './src/model/TimePreferences';
 
 const EXAMPLE_ACT = function mockExternalServiceReturn(): number {
-    return 500;
+    return 200;
 };
 
 const EXAMPLE_TIME_PREFERENCES: TimePreferences = {
